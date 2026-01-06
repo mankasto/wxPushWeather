bf86a3d93ad01203b30a8480ce98e0ed4c05e048
Kailang Ma <61776666+mankasto@users.noreply.github.com>
2026-01-07 00:32:05 +0800

    Update main.py
    add apiHost

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ import os
 
 # 哪天在一起的
 start_date = os.environ['START_DATE']
-# 和风天气key
+# 和风天气key, host
 appKey = os.environ['APP_KEY']
+appHost = os.environ['APP_Host']
 # 生日
 birthday = os.environ['BIRTHDAY']
 # 微信公众号的appid和app_secret
@@ -43,13 +44,13 @@ params = {
 }
 
 # 根据城市名查找地理位置
-url = "https://kn6x88q3g7.re.qweatherapi.com/geo/v2/city/lookup"
+url = f"https://{appHost}/geo/v2/city/lookup"
 resp_json = json.loads(requests.get(url, params, headers=headers).text)
 city_id = resp_json["location"][0]["id"]
 params["location"] = city_id
 
 # 根据城市地理位置获取当前实时天气
-url = "https://kn6x88q3g7.re.qweatherapi.com/v7/weather/now"
+url = f"https://{appHost}/v7/weather/now"
 realtime_json = json.loads(requests.get(url, params, headers=headers).text)
 # 实时天气状况
 realtime = realtime_json["now"]
@@ -57,7 +58,7 @@ realtime = realtime_json["now"]
 now_temperature = realtime["temp"] + "℃" + realtime["text"]
 
 # 根据城市地理位置获取3天天气状况
-url = "https://kn6x88q3g7.re.qweatherapi.com/v7/weather/3d"
+url = f"https://{appHost}/v7/weather/3d"
 day_forecast_json = json.loads(requests.get(url, params, headers=headers).text)
 
 # -----------------------今天天气状况-----------------------------
